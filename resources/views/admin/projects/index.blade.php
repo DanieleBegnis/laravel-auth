8e532e42fac3ad203019f9b6c4a5210f8e57537c
DanieleBegnis <daniele.begnis@gmail.com>
2024-05-30 18:41:07 +0200

    feat: add crud index

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h3>Ecco qui tutte le tue Repositories!</h3>
+    <table class="table table-dark table-striped">
+        <thead>
+            <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Slug</th>
+                <th scope="col">Client Name</th>
+                <th scope="col">Created At</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr>
+                    <td>{{ $project->id }}</td>
+                    <td>{{ $project->name }}</td>
+                    <td>{{ $project->slug }}</td>
+                    <td>{{ $project->client_name }}</td>
+                    <td>{{ $project->created_at }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
