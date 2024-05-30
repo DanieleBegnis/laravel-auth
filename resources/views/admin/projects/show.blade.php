f7abb3c275f9a90da9ae182b9390af6a901e97a1
DanieleBegnis <daniele.begnis@gmail.com>
2024-05-30 18:59:37 +0200

    feat: add show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,5 +1,9 @@
 @extends('layouts.admin')
 
 @section('content')
-    testtest test show
+    <h3>{{ $project->name }}</h3>
+    <p>{{ $project->summary }}</p>
+    <p>Progetto per: {{ $project->client_name }}</p>
+    <p>Creato il: {{ $project->created_at }}</p>
+    <p>Aggiornato il: {{ $project->updated_at }}</p>
 @endsection
