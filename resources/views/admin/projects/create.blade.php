2195a500062796c1221a45a3f8b2c1c141bc0d31
DanieleBegnis <daniele.begnis@gmail.com>
2024-06-04 15:57:06 +0200

    add file to form and enctype

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <h3>Crea un nuovo Progetto</h3>
-    <form action="{{ route('admin.projects.store') }}" method="POST">
+    <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
 
         <div class="mb-3">
@@ -16,21 +16,25 @@
 
         <div class="mb-3">
             <label for="client_name" class="form-label">Nome del Cliente</label>
-            <input type="text" class="form-control" id="client_name" name="client_name"
-                aria-describedby="emailHelp" value="{{ old('client_name') }}">
+            <input type="text" class="form-control" id="client_name" name="client_name" aria-describedby="emailHelp"
+                value="{{ old('client_name') }}">
         </div>
         @error('client_name')
             <div class="alert alert-danger">{{ $message }}</div>
         @enderror
 
         <div class="form-floating">
-            <textarea class="form-control" id="summary" name="summary" style="height: 100px"
-                value="{{ old('summary') }}"></textarea>
+            <textarea class="form-control" id="summary" name="summary" style="height: 100px" value="{{ old('summary') }}"></textarea>
             <label for="summary">Descrizione del Progetto</label>
         </div>
         @error('summary')
             <div class="alert alert-danger">{{ $message }}</div>
         @enderror
+
+        <div class="mb-3">
+            <label for="project_thumbnail" class="form-label">Inserisci il Thumbnail del progetto</label>
+            <input class="form-control" type="file" id="project_thumbnail" name="project_thumbnail">
+        </div>
 
         <button type="submit" class="btn btn-primary my-3">Salva</button>
     </form>
